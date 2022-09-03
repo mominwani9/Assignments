9921f682f2b6d27fb1e7cea2935eab7a096f5e7f
mominwani9 <67506632+mominwani9@users.noreply.github.com>
2022-09-04 00:05:43 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,55 @@
+/*============================ finding the factorials========================
+
+let n=7
+let sum=1;
+for (i=1; i<=n; i++){
+
+    sum=sum*i
+}
+
+
+console.log(`Factoial of ${n} is ${sum}`)
+
+*/
+
+
+/*
+==================================================================================
+
+Write a JavaScript program to convert temperatures to and from Celsius,
+Fahrenheit.
+[ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
+Fahrenheit ]
+Expected Output :
+60°C is 140 °F
+45°F is 7.222222222222222°C
+
+
+
+let c=60
+// c=((f-32)/9)*5
+// console.log(c)
+
+f=((c/5)*9)+32
+console.log(f)
+
+
+*/
+
+
+
+
+
+/*====================Write a program to find whether a given year is a leap year or not.=================
+
+            year=1988
+
+            if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ){
+                console.log(`${year} is leap year`)
+
+            }
+            else{
+
+                console.log(`${year} is not leap year`)
+            }    
+*/
